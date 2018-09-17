a6986796de4dba87bd6d037b90ed10c597c356e1
周志衠Jed <jed@miniasp.com>
2018-09-17 10:42:44 +0800

    Add IdentityServer4.AccessTokenValidation 套件. 設定Resource組態

--- ResourceServer/Startup.cs
+++ ResourceServer/Startup.cs
@@ -25,22 +25,24 @@ namespace ResourceServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-        }
+            services.AddMvcCore()
+                 .AddAuthorization()
+                 .AddJsonFormatters();
+
+            services.AddAuthentication("Bearer")
+                    .AddIdentityServerAuthentication(options =>
+                    {
+                            options.Authority = "http://localhost:5000";
+                            options.RequireHttpsMetadata = false;
+                            options.ApiName = "api1";
+                        });
+                    }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
+            app.UseAuthentication();
 
-            app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
